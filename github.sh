99942c61a99207c216b83f1ccbab2d442efaef43
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-01-07 15:56:38 -0500

    Create github.sh

---
+++ github.sh
@@ -0,0 +1,15 @@
+
+
+## Github
+```
+sudo apt-get install git
+git config --global user.name "Shicheng-Guo"
+git config --global user.email "shicheng.guo@hotmail.com"
+cd /home/shg047/apt/Github/Ubuntu
+echo "# Ubuntu" >> README.md
+git init
+git add README.md
+git commit -m "readme"
+git remote add origin https://github.com/Shicheng-Guo/Ubuntu.git
+git push -u origin master
+```
